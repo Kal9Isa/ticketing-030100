0fbe4ea2ba3585052a7f97cb00a8c14fe5370c8c
kal9isa <docalisa_it@yahoo.com>
2021-03-24 13:16:35 -0700

    done useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
+import useRequest from "../../hooks/use-request";
+
 
 export default () => {
   const [email, setEmail] = useState("");
@@ -40,14 +42,7 @@ export default () => {
         />
       </div>
       {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops ...</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
+        
       )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
