f88f253f6cb65063c4ac357e636db7cbd7b4fe3b
kal9isa <docalisa_it@yahoo.com>
2021-03-24 02:29:44 -0700

    create user done

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
@@ -19,12 +19,20 @@ router.post(
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+
     const { email, password } = req.body;
 
-    console.log("Creating user ...");
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
