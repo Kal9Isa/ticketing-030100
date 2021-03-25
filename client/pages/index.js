a607c32b40c2b27ce5ed7bd6397ba971c1df08b0
kal9isa <docalisa_it@yahoo.com>
2021-03-25 09:17:41 -0700

    send ssr reqs to nginx with cookies for currentuser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,27 @@
-export default () => {
+import axios from "axios";
+// hooks only work inside React components
+
+const LandingPage = ({ currentuser }) => {
   return <h1>This is Landing Page!</h1>;
 };
+
+// Runs on the server before rendering
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // We on server
+    // URL can be shortend by using external service name in k8s
+    const { data } = await axios.get(
+      "http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // We on browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+export default LandingPage;
