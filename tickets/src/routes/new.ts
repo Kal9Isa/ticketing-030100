561c480d86ac1b269e707c362322f7523021d9a0
kal9isa <docalisa_it@yahoo.com>
2021-03-26 10:58:15 -0700

    validate incoming params on new ticket

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,21 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@kal9isa-tickets/common";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@kal9isa-tickets/common";
 
 const router = express.Router();
 
 router.post(
   "/api/tickets",
   requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .not()
+      .isEmpty()
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be valid"),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     res.sendStatus(200);
   }
