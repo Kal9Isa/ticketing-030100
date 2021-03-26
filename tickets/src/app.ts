6c4562908557110d85730f111d28b4a8fbe6f9e4
kal9isa <docalisa_it@yahoo.com>
2021-03-26 09:47:07 -0700

    added auth check middlewares on create ticket

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,12 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@kal9isa-tickets/common";
+import {
+  errorHandler,
+  NotFoundError,
+  requireAuth,
+  currentUser,
+} from "@kal9isa-tickets/common";
 
 import { createTicketRouter } from "./routes/new";
 
@@ -18,6 +23,8 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -6,6 +6,16 @@ it("As a route handler listening to /api/tickets for POST requests", async () =>
 
   expect(response.status).not.toEqual(404);
 });
-it("Can only be accessed if the user is signed in", async () => {});
+
+it("Can only be accessed if the user is signed in", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401);
+});
+
+it("Returns an status other than 401 if the user is signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+
+  expect(response.status).not.toEqual(401);
+});
+
 it("Returns an error is an invalid title or price is provided", async () => {});
 it("Creates a ticket with valid parameters", async () => {});
