076b577805daa14aafcb940ca51bfa02fba63ffe
kal9isa <docalisa_it@yahoo.com>
2021-03-30 08:11:12 -0700

    added orders scaffhold

---
+++ orders/src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import {
+  errorHandler,
+  NotFoundError,
+  requireAuth,
+  currentUser,
+} from "@hiroit/common";
+
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+// Allow Ingress NGINX
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    // No encryption
+    signed: false,
+    // over HTTPS
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(createTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
